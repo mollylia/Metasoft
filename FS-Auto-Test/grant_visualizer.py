58b1930f4db72fdc929fe4ac85330221ac2d7d27
mollylia <mollylia@student.ubc.ca>
2024-02-14 10:53:37 -0800

    Scroll into view before clicking search

--- FS-Auto-Test/grant_visualizer.py
+++ FS-Auto-Test/grant_visualizer.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
 
 # Finds and goes to 'Grant Analyzer' page from user dashboard
@@ -49,65 +49,69 @@ def fs_help():
 
 
 # Sets the view mode
+# TODO: page not fully loading
 def view_mode(mode):
     print("  setting view mode")
     view_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayMode"))
     view_selector.select_by_visible_text(mode)
 
-    # wait until page becomes stable before continuing
+    try:
+        print("  waiting for element to be clickable")
+        WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_btnUpdateSearch")))
+    except StaleElementReferenceException:
+        print("  StaleElementReferenceException")
+        WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_btnUpdateSearch")))
 
 
-# # Sets dependent variable for map view (second dropdown menu) and chart view (third dropdown menu)
-# # REQUIRES: map view or chart view to be selected
-# def mode_set_dependent_variable(display):
-#     try:
-#         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
-#         display_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField"))
-#         display_selector.select_by_visible_text(display)
-#     except NoSuchElementException:
-#         print("Grant Visualizer: mode_set_dependent_variable is not available for this view mode")
-#
-#
-# # Sets grant direction for map view (third dropdown menu)
-# # REQUIRES: map view to be selected
-# def map_set_grant_direction(direction):
-#     try:
-#         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
-#         direction_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlMapGrantsDirection"))
-#         direction_selector.select_by_visible_text(direction)
-#     except NoSuchElementException:
-#         print("Grant Visualizer: map_set_grant_direction is not available for this view mode")
-#
-#
-# # Sets independent variable for chart view (second dropdown menu)
-# # REQUIRES: chart view to be selected
-# def chart_set_independent_variable(variable):
-#     try:
-#         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
-#         variable_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlChartFacet"))
-#         variable_selector.select_by_visible_text(variable)
-#     except NoSuchElementException:
-#         print("Grant Visualizer: chart_set_independent_variable is not available for this view mode")
-#
-#
-# # Sets chart type for chart view (fourth dropdown menu)
-# # REQUIRES: chart view to be selected
-# def chart_type(chart):
-#     try:
-#         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
-#         type_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlChartType_SingleSeries"))
-#         type_selector.select_by_visible_text(chart)
-#     except NoSuchElementException:
-#         print("Grant Visualizer: chart_type is not available for this view mode")
+# Sets dependent variable for map view (second dropdown menu) and chart view (third dropdown menu)
+# REQUIRES: map view or chart view to be selected
+def mode_set_dependent_variable(display):
+    try:
+        WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
+        display_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField"))
+        display_selector.select_by_visible_text(display)
+    except NoSuchElementException:
+        print("Grant Visualizer: mode_set_dependent_variable is not available for this view mode")
+
+
+# Sets grant direction for map view (third dropdown menu)
+# REQUIRES: map view to be selected
+def map_set_grant_direction(direction):
+    try:
+        WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
+        direction_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlMapGrantsDirection"))
+        direction_selector.select_by_visible_text(direction)
+    except NoSuchElementException:
+        print("Grant Visualizer: map_set_grant_direction is not available for this view mode")
+
+
+# Sets independent variable for chart view (second dropdown menu)
+# REQUIRES: chart view to be selected
+def chart_set_independent_variable(variable):
+    try:
+        WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
+        variable_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlChartFacet"))
+        variable_selector.select_by_visible_text(variable)
+    except NoSuchElementException:
+        print("Grant Visualizer: chart_set_independent_variable is not available for this view mode")
+
+
+# Sets chart type for chart view (fourth dropdown menu)
+# REQUIRES: chart view to be selected
+def chart_type(chart):
+    try:
+        WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
+        type_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlChartType_SingleSeries"))
+        type_selector.select_by_visible_text(chart)
+    except NoSuchElementException:
+        print("Grant Visualizer: chart_type is not available for this view mode")
 
 
 # Sets group by for summary view (second dropdown menu)
 # REQUIRES: summary view to be selected
 def summary_group_by(grouping):
     try:
-        print("  summary: setting group by")
-        WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
+        WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
         grouping_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlSummaryFacet"))
         grouping_selector.select_by_visible_text(grouping)
     except NoSuchElementException:
@@ -118,7 +122,7 @@ def summary_group_by(grouping):
 # REQUIRES: list view to be selected
 def list_group_by(grouping):
     try:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
+        WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlDisplayField")))
         grouping_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_ddlGrantListBy"))
         grouping_selector.select_by_visible_text(grouping)
     except NoSuchElementException:
@@ -153,7 +157,7 @@ def add_to_province(province):
 
 # Sets initial 'granting year' search criteria at the bottom of page
 def set_year(year):
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_lstYear")))
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_lstYear")))
     year_selector = Select(driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_lstYear"))
     year_selector.deselect_all()                                         # deselects the initial 'all years' option
     year_selector.select_by_visible_text(year)
@@ -206,7 +210,8 @@ def set_funder_designation(first, second, third):
 
 # Clicks the search button at the bottom of page
 def search():
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    element_into_view = driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_Panel1")
+    driver.execute_script("arguments[0].scrollIntoView();", element_into_view)
     driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_btnSearch").click()
 
 
@@ -219,13 +224,13 @@ def fs_reset():
 # Returns the number of grants found from search
 def get_number_results():
     try:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_lblTotalGrants")))
+        WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_lblTotalGrants")))
         summary = driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_lblTotalGrants").text
         return summary
     except Exception as e:
         print(f"An error has occurred: {e}")
         print("Refreshing page...")
         driver.refresh()
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_lblTotalGrants")))
+        WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_lblTotalGrants")))
         summary = driver.find_element(By.ID, "ctl00_ctl00_TabContentPlaceHolder_FindFundersContentPlaceHolder_lblTotalGrants").text
         return summary
