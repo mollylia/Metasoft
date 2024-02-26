f118a402648b616b37873583bdd6a64e8641c51f
mollylia <mollylia@student.ubc.ca>
2024-02-26 12:39:30 -0800

    Updated foundation profile keyword search test assertions

--- FS-Auto-Test/foundation_profile_keyword_search_test.py
+++ FS-Auto-Test/foundation_profile_keyword_search_test.py
@@ -15,25 +15,24 @@ class FoundationProfileKeywordSearchTest(unittest.TestCase):
     def test_keyword_search_gates(self):
         foundation_profile_keyword_search.set_keyword("gates")
         foundation_profile_keyword_search.search()
-        self.assertEqual("4", foundation_profile_keyword_search.get_number_results())
+        self.assertTrue(4*0.8 <= foundation_profile_keyword_search.get_number_results() <= 4*1.2, "Result is more than 20% off")
 
     def test_foundation_search_ford(self):
         foundation_profile_keyword_search.set_foundation_name("ford")
         foundation_profile_keyword_search.search()
-        self.assertEqual("1", foundation_profile_keyword_search.get_number_results())
+        self.assertTrue(1*0.8 <= foundation_profile_keyword_search.get_number_results() <= 1*1.2, "Result is more than 20% off")
 
     def test_foundation_search_foundation(self):
         foundation_profile_keyword_search.set_foundation_name("foundation")
         foundation_profile_keyword_search.search()
-        self.assertEqual("6,955", foundation_profile_keyword_search.get_number_results())
+        self.assertTrue(6955*0.8 <= foundation_profile_keyword_search.get_number_results() <= 6955*1.2, "Result is more than 20% off")
 
     def test_keyword_and_foundation(self):
         foundation_profile_keyword_search.set_foundation_name("foundation")
         foundation_profile_keyword_search.set_keyword("law")
         foundation_profile_keyword_search.search()
-        self.assertEqual("29", foundation_profile_keyword_search.get_number_results())
+        self.assertTrue(29*0.8 <= foundation_profile_keyword_search.get_number_results() <= 29*1.2, "Result is more than 20% off")
 
 
 if __name__ == '__main__':
-    print("TESTING: foundation profile keyword search")
     unittest.main()
