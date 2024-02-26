4cb0681645c51b7eb0eb009396296310b91ae68b
mollylia <mollylia@student.ubc.ca>
2024-02-26 14:07:32 -0800

    Added result variable for foundation search tests

--- FS-Auto-Test/foundation_search_test.py
+++ FS-Auto-Test/foundation_search_test.py
@@ -9,50 +9,61 @@ class FoundationSearchTest(unittest.TestCase):
         main.fs_login(main.fs_username, main.fs_password)
         foundation_search.navigate()
 
+    def tearDown(self):
+        print("\n")
+
     def test_foundation_search_ford(self):
-        foundation_search.set_foundation_name("ford")
+        original_result = 1
+        foundation_search.set_foundation_name_with("ford")
         foundation_search.search()
-        self.assertEqual("1", foundation_search.get_number_results())
+        self.assertTrue(original_result*0.8 <= foundation_search.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_designation_private_sort_asset(self):
+        original_result = 6785
         foundation_search.set_funder_designation("Private Foundations", "", "")
         foundation_search.sort_by("Total Assets")
         foundation_search.search()
-        self.assertEqual("6,661", foundation_search.get_number_results())
+        self.assertTrue(original_result*0.8 <= foundation_search.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_two_designations(self):
-        foundation_search.set_foundation_name("foundation")
+        original_result = 204
+        foundation_search.set_foundation_name_with("foundation")
         foundation_search.set_funder_designation("Charitable Organizations", "Community Foundations", "")
         foundation_search.search()
-        self.assertEqual("205", foundation_search.get_number_results())
+        self.assertTrue(original_result*0.8 <= foundation_search.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_giving_interest_school(self):
+        original_result = 2890
         foundation_search.set_giving_interest("School")
         foundation_search.search()
-        self.assertEqual("2,860", foundation_search.get_number_results())
+        self.assertTrue(original_result*0.8 <= foundation_search.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_giving_interest_school_OR_house(self):
-        foundation_search.set_giving_interest("\"School\" OR \"House\"")
+        original_result = 2890
+        foundation_search.set_giving_interest("School OR House")
         foundation_search.search()
-        self.assertEqual("2,860", foundation_search.get_number_results())
+        self.assertTrue(original_result*0.8 <= foundation_search.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_giving_interest_school_OR_athletes(self):
-        foundation_search.set_giving_interest("\"School\" OR \"Athletes\"")
+        original_result = 2911
+        foundation_search.set_giving_interest("School OR Athletes")
         foundation_search.search()
-        self.assertEqual("2,879", foundation_search.get_number_results())
+        self.assertTrue(original_result*0.8 <= foundation_search.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_giving_interest_school_AND_athletes(self):
-        foundation_search.set_giving_interest("\"School\" AND \"Athletes\"")
+        original_result = 80
+        foundation_search.set_giving_interest("School AND Athletes")
         foundation_search.search()
-        self.assertEqual("77", foundation_search.get_number_results())
+        self.assertTrue(original_result*0.8 <= foundation_search.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_multiple_criteria(self):
-        foundation_search.set_foundation_name("foundation")
+        original_result = 1539
+        foundation_search.set_foundation_name_with("foundation")
         foundation_search.set_funder_designation("Private Foundations", "", "")
         foundation_search.sort_by("Total Assets")
         foundation_search.set_giving_interest("School")
         foundation_search.search()
-        self.assertEqual("1,509", foundation_search.get_number_results())
+        self.assertTrue(original_result*0.8 <= foundation_search.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
 
 if __name__ == '__main__':
