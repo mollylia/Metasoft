3e359d0619bf52d06c58771183b500b3b8f265c2
mollylia <mollylia@student.ubc.ca>
2024-02-27 09:39:35 -0800

    Added result variable for grant visualizer tests

--- FS-Auto-Test/grant_visualizer_test.py
+++ FS-Auto-Test/grant_visualizer_test.py
@@ -10,9 +10,11 @@ class GrantVisualizerTest(unittest.TestCase):
         grant_visualizer.navigate()
 
     def test_no_criteria(self):
-        self.assertEqual("2,172,407", grant_visualizer.get_number_results())
+        original_result = 2172407
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_multiple_criteria(self):
+        original_result = 2271
         grant_visualizer.set_to_province("Alberta")
         grant_visualizer.add_to_province("Manitoba")
         grant_visualizer.set_year("2020")
@@ -23,77 +25,91 @@ class GrantVisualizerTest(unittest.TestCase):
         grant_visualizer.add_size("$10,000 to $24,999")
         grant_visualizer.set_funder_designation("Private Foundations", "Charitable Organizations", "")
         grant_visualizer.search()
-        self.assertEqual("2,271", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_map(self):
+        original_result = 2172407
         grant_visualizer.view_mode("Map")
-        self.assertEqual("2,172,407", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_map_first_selector(self):
+        original_result = 2172407
         grant_visualizer.view_mode("Map")
         grant_visualizer.mode_set_dependent_variable("Number of Grants")
-        self.assertEqual("2,172,407", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_map_second_selector(self):
+        original_result = 2277385
         grant_visualizer.view_mode("Map")
         grant_visualizer.map_set_grant_direction("Grants Given")
-        self.assertEqual("2,277,385", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_map_all_selectors(self):
+        original_result = 2277385
         grant_visualizer.view_mode("Map")
         grant_visualizer.mode_set_dependent_variable("Number of Grants")
         grant_visualizer.map_set_grant_direction("Grants Given")
-        self.assertEqual("2,277,385", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_chart(self):
+        original_result = 2285071
         grant_visualizer.view_mode("Chart")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_chart_first_selector(self):
+        original_result = 2285071
         grant_visualizer.view_mode("Chart")
         grant_visualizer.chart_set_independent_variable("Category")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_chart_second_selector(self):
+        original_result = 2285071
         grant_visualizer.view_mode("Chart")
         grant_visualizer.mode_set_dependent_variable("Number of Grants")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_chart_third_selector(self):
+        original_result = 2285071
         grant_visualizer.view_mode("Chart")
         grant_visualizer.chart_type("Horizontal Bar (3D)")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_chart_fourth_selector(self):
+        original_result = 2285071
         grant_visualizer.view_mode("Chart")
         grant_visualizer.mode_set_dependent_variable("Grant Amount / Number of Grants")
         grant_visualizer.chart_set_bubble("Bubble size = Grant Amount")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_chart_all_selectors(self):
+        original_result = 2285071
         grant_visualizer.view_mode("Chart")
         grant_visualizer.chart_set_independent_variable("Category")
         grant_visualizer.mode_set_dependent_variable("Number of Grants")
         grant_visualizer.chart_type("Horizontal Bar (3D)")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_summary(self):
+        original_result = 2285071
         grant_visualizer.view_mode("Summary")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_summary_all_selectors(self):
+        original_result = 2285071
         grant_visualizer.view_mode("Summary")
         grant_visualizer.summary_group_by("Grant Size")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_list(self):
+        original_result = 2285071
         grant_visualizer.view_mode("List")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
     def test_list_all_selectors(self):
+        original_result = 2285071
         grant_visualizer.view_mode("List")
         grant_visualizer.list_group_by("By Recipients")
-        self.assertEqual("2,285,071", grant_visualizer.get_number_results())
+        self.assertTrue(original_result*0.8 <= grant_visualizer.get_number_results() <= original_result*1.2, "Result is more than 20% off")
 
 
 if __name__ == '__main__':
