f8f4f21a9a5163c2b3c506758ce2932547cb17e7
mollylia <mollylia@student.ubc.ca>
2024-03-01 13:33:39 -0800

    Creates new directory and file for crawled pages

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/ford_foundation_spider.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/ford_foundation_spider.py
@@ -1,3 +1,5 @@
+import os.path
+
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
@@ -7,17 +9,30 @@ class FordFoundationSpider(CrawlSpider):
     allowed_domains = ["fordfoundation.org"]
     start_urls = ["https://www.fordfoundation.org/"]
 
-    # custom_settings = {
-    #     "DEPTH_LIMIT": 3
-    # }
+    custom_settings = {
+        "DEPTH_LIMIT": 3
+    }
 
     rules = (
-        Rule(LinkExtractor(allow="work/investing-in-individuals"), callback="parse_item"),   # TODO: change rules later
-
+        Rule(LinkExtractor(allow="work/"), callback="parse_item", follow=True),   # TODO: change rules later
+        # Rule(LinkExtractor(), callback="parse_item", follow=True)
     )
 
     def parse_item(self, response):
-        yield {
-            "title": response.css(".alignwide h1::text").get()
-        }
+        title = response.css(".alignwide h1::text").get()
 
+        # Creates a directory to save crawled pages if it doesn't exist
+        if not os.path.exists("fordfoundation.org"):
+            os.mkdir("fordfoundation.org")
+
+        url_path = response.request.url
+        ford_directory = url_path[31:-1]
+        name_split = url_path.split("/")
+        file_name = name_split[len(name_split)-2] + ".html"
+
+
+        yield {
+            "directory": ford_directory,
+            "file": file_name,
+            "title": title
+        }
