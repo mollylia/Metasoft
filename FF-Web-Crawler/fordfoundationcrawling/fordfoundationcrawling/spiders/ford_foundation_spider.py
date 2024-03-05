5a7f30cc47acfcc84bdead8dd51352bfdfa2bced
mollylia <mollylia@student.ubc.ca>
2024-03-05 12:09:15 -0800

    Names all the main files under each subfolder index.html

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/ford_foundation_spider.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/ford_foundation_spider.py
@@ -7,44 +7,54 @@ from bs4 import BeautifulSoup
 
 
 class FordFoundationSpider(CrawlSpider):
-    name = 'fordfoundation'
-    allowed_domains = ['fordfoundation.org']
-    start_urls = ['https://www.fordfoundation.org/']
+    name = "fordfoundation"
+    allowed_domains = ["fordfoundation.org"]
+    start_urls = ["https://www.fordfoundation.org/"]
 
     custom_settings = {
-        'DEPTH_LIMIT': 1
+        "DEPTH_LIMIT": 3
     }
 
     rules = (
-        Rule(LinkExtractor(allow='work/challenging-inequality/disability-rights'), callback='parse_item', follow=True),   # TODO: change rules later
+        Rule(LinkExtractor(allow="work/"), callback="parse_item", follow=True),
+        # Rule(LinkExtractor(allow="work/challenging-inequality/disability-rights"), callback="parse_item", follow=True),   # TODO: change rules later
         # Rule(LinkExtractor(), callback="parse_item", follow=True)
     )
 
     def remove_media_files(self, html):
         soup = BeautifulSoup(html, 'html.parser')
-
-        for data in soup(['style', 'script', 'meta', 'img', 'button', 'input', 'figure', 'picture', 'select']):
+        for data in soup(["style", "script", "meta", "img", "button", "input", "figure", "picture", "select"]):
             data.decompose()
 
         return soup.prettify()
-        # return soup
-        # return ' '.join(soup.stripped_strings)
+
+    def get_file_name(self, url, crawl_depth):
+        if crawl_depth == 1:
+            return "index.html"
+
+        name_split = url.split("/")
+        file_name = name_split[len(name_split)-2] + ".html"
+        return file_name
 
     def parse_item(self, response):
-        title = response.css('.alignwide h1::text').get()
+        title = response.css(".alignwide h1::text").get()
 
         # Creates a directory to save crawled pages if it doesn't exist
-        if not os.path.exists('fordfoundation.org'):
-            os.mkdir('fordfoundation.org')
+        if not os.path.exists("fordfoundation.org"):
+            os.mkdir("fordfoundation.org")
+
 
         url = response.request.url
+        crawl_depth = response.meta["depth"]
+        file_name = self.get_file_name(url, crawl_depth)
         page = requests.get(url)
         content = self.remove_media_files(page.content.decode())
-        # content = self.remove_media_files(page.content)
 
-        name_split = url.split('/')
-        file_name = name_split[len(name_split)-2] + '.html'
+        # with open(file_name, 'w') as html_file:
+        #     html_file.write(str(content))
 
-        with open(file_name, 'w') as html_file:
-            html_file.write(str(content))
-            # html_file.write(content)
+        yield {
+            "file name": file_name,
+            "url": url,
+            "crawl depth": crawl_depth
+        }
