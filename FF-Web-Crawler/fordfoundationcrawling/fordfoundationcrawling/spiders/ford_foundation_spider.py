611d8f66531d482e2abee8a44531fc926a057b87
mollylia <mollylia@student.ubc.ca>
2024-03-11 11:16:44 -0700

    Updated directory and file names for crawled pages

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/ford_foundation_spider.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/ford_foundation_spider.py
@@ -17,10 +17,24 @@ class FordFoundationSpider(CrawlSpider):
 
     rules = (
         # Rule(LinkExtractor(allow="news-and-stories/"), callback="parse_item", follow=True),
-        # Rule(LinkExtractor(allow="work/challenging-inequality/"), callback="parse_item", follow=True),
-        Rule(LinkExtractor(allow="work/challenging-inequality/disability-rights"), callback="parse_item", follow=True),
+        Rule(LinkExtractor(allow="work/challenging-inequality/"), callback="parse_item", follow=True),
+        # Rule(LinkExtractor(allow="work/challenging-inequality/disability-rights"), callback="parse_item", follow=True),
         # Rule(LinkExtractor(), callback='parse_item', follow=True),
     )
+
+    def get_file_name(self, url):
+        if url == "https://www.fordfoundation.org/":
+            return "index.html"
+
+        name_split = url[31:].split('/')
+
+        if "page" in name_split:
+            page_index = name_split.index('page')
+            file_name = f"{name_split[page_index-1]}-{name_split[page_index]}-{name_split[page_index+1]}.html"
+            return file_name
+        else:
+            file_name = f"{name_split[len(name_split)-2]}.html"
+            return file_name
 
     def get_page_content(self, html):
         # Removes media files and CSS selectors
@@ -45,25 +59,13 @@ class FordFoundationSpider(CrawlSpider):
 
         return soup.prettify()
 
-    def get_file_name(self, url, crawl_depth):
-        if crawl_depth == 1:
-            return "index.html"
-
-        name_split = url[31:].split('/')
-
-        if "page" in name_split:
-            page_index = name_split.index('page')
-            file_name = f"{name_split[page_index-1]}-{name_split[page_index]}-{name_split[page_index+1]}.html"
-            return file_name
-        else:
-            file_name = f"{name_split[len(name_split) - 2]}.html"
-            return file_name
-
-    def go_to_directory(self, url, crawl_depth):
+    def go_to_directory(self, url):
         os.chdir('fordfoundation.org')
         directories = url[31:-1].split('/')
 
-        if (crawl_depth != 1) or ("?filter_news_press_type=new" in directories):
+        # if len(directories) > 1:
+        directories.pop()
+        if "?filter_news_press_type=new" in directories:
             directories.pop()
 
         for directory in directories:
@@ -72,18 +74,22 @@ class FordFoundationSpider(CrawlSpider):
 
             os.chdir(directory)
 
-    def save_file(self, file_name, content):
-        with open(file_name, 'w') as html_file:
-            html_file.write(str(content))
-
-    def return_from_directory(self, url, crawl_depth):
+    def return_from_directory(self, url):
         directories = url[31:-1].split('/')
 
-        if (crawl_depth != 1) or ("?filter_news_press_type=new" in directories):
+        # if len(directories) > 1:
+        directories.pop()
+        if "?filter_news_press_type=new" in directories:
             directories.pop()
 
         for directory in directories:
             os.chdir('..')
+
+
+    def save_file(self, file_name, content):
+        with open(file_name, 'w') as html_file:
+            html_file.write(str(content))
+
 
     def parse_item(self, response):
         os.chdir('..')
@@ -95,19 +101,19 @@ class FordFoundationSpider(CrawlSpider):
         # Gets the file name for the pages
         url = response.request.url
         crawl_depth = response.meta['depth']
-        file_name = self.get_file_name(url, crawl_depth)
+        file_name = self.get_file_name(url)
 
         # Get the content from the crawled pages
         page = requests.get(url)
         content = self.get_page_content(page.content.decode())
 
         # Saves the content in a directory corresponding to the website
-        self.go_to_directory(url, crawl_depth)
+        self.go_to_directory(url)
         self.save_file(file_name, content)
-        self.return_from_directory(url, crawl_depth)
+        self.return_from_directory(url)
 
-        # yield {
-        #     "file name": file_name,
-        #     "url": url,
-        #     "crawl depth": crawl_depth
-        # }
+        yield {
+            "file name": file_name,
+            "url": url,
+            "crawl depth": crawl_depth
+        }
