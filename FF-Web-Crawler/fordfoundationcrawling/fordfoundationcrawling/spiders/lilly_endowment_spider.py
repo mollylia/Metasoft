39c020f5f73db58074e894ec0d430cac2b3f0585
mollylia <mollyliao0803@gmail.com>
2024-03-18 16:50:45 -0700

    Scraping causes 403 forbidden error

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/lilly_endowment_spider.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/lilly_endowment_spider.py
@@ -17,7 +17,7 @@ class LillyEndowmentSpider(CrawlSpider):
     os.chdir('lillyendowment.org')
 
     custom_settings = {
-        'DEPTH_LIMIT': 2
+        'DEPTH_LIMIT': 1
     }
 
     rules = (
@@ -32,12 +32,25 @@ class LillyEndowmentSpider(CrawlSpider):
             file_name = f"{name_split.pop()}.html"
             return file_name
 
+    # def get_page_content(self, html):
+    #     soup = BeautifulSoup(html, 'html.parser')
+    #     for data in soup(['script']):
+    #         data.decompose()
+    #
+    #     return soup.prettify()
 
     def parse_item(self, response):
         url = response.request.url
 
         # Gets the file name for the pages
         file_name = self.get_file_name(url)
+
+        # Get the content from the crawled pages
+        # page = requests.get(url)
+        # content = self.get_page_content(page.content.decode())
+        #
+        # with open(file_name, 'w') as html_file:
+        #     html_file.write(str(content))
 
         yield {
             "file name": file_name,
