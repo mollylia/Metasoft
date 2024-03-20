0b8bcf7b1cad64008848f6c78bb4002ece17be40
mollylia <mollyliao0803@gmail.com>
2024-03-20 15:11:18 -0700

    Saves crawled pages

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/phil_mcnamee.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/phil_mcnamee.py
@@ -1,7 +1,9 @@
 import os.path
+import requests
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
 
 
 class PhilMcNameeSpider(CrawlSpider):
@@ -31,12 +33,47 @@ class PhilMcNameeSpider(CrawlSpider):
             file_name = f"{name_split.pop()}.html"
             return file_name
 
+    def get_page_content(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        for data in soup(['script', 'meta', 'style', 'img', 'iframe']):
+            data.decompose()
+
+        # Replaces href URL with file path
+        for a in soup.findAll('a'):
+            if 'href' in a.attrs:
+                url = a['href']
+                file_path = "/solitudenaturereserve.com/"
+
+                if url == 'https://solitudenaturereserve.com/':
+                    a['href'] = f"{file_path}index.html"
+
+                elif ('https://solitudenaturereserve.com/' in url) and ('png' not in url):
+                    directories = url.split('/')[3:]
+                    if directories[len(directories)-1] == '':
+                        directories.pop()
+
+                    file_name = directories.pop()
+                    for directory in directories:
+                        file_path += f"{directory}/"
+
+                    file_path += f"{file_name}.html"
+                    a['href'] = file_path
+
+        return soup.prettify()
+
     def parse_item(self, response):
         url = response.request.url
 
         # Gets the file name for the pages
         file_name = self.get_file_name(url)
         crawl_depth = response.meta['depth']
+
+        # Get the content from the crawled pages
+        page = requests.get(url)
+        content = self.get_page_content(page.content.decode())
+
+        with open(file_name, 'w') as html_file:
+            html_file.write(str(content))
 
         yield {
             "file name": file_name,
