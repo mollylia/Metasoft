a57d3199279335358f45777e2d5930c32bff7a95
mollylia <mollyliao0803@gmail.com>
2024-03-21 17:55:03 -0700

    Saves crawled pages in directory corresponding to website

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/azrieli_foundation.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/azrieli_foundation.py
@@ -11,16 +11,6 @@ class AzrieliFoundationSpider(CrawlSpider):
     allowed_domains = ['azrielifoundation.org']
     start_urls = ['https://azrielifoundation.org/']
 
-    # TODO: move this to parse_item
-    os.chdir('..')
-    if not os.path.exists('azrielifoundation.org'):
-        os.mkdir('azrielifoundation.org')
-    os.chdir('azrielifoundation.org')
-
-    custom_settings = {
-        'DEPTH_LIMIT': 1
-    }
-
     rules = (
         Rule(LinkExtractor(), callback='parse_item', follow=True),
     )
@@ -28,8 +18,13 @@ class AzrieliFoundationSpider(CrawlSpider):
     def get_file_name(self, url):
         if url == 'https://azrielifoundation.org/':
             return "index.html"
+        elif url == 'https://memoirs.azrielifoundation.org/':
+            return "index-memoirs.html"
         else:
-            name_split = url[:-1].split('/')
+            name_split = url.split('/')
+            if name_split[len(name_split)-1] == '':
+                name_split = url[:-1].split('/')
+
             file_name = f"{name_split.pop()}.html"
             return file_name
 
@@ -52,14 +47,38 @@ class AzrieliFoundationSpider(CrawlSpider):
                     if directories[len(directories)-1] == '':
                         directories.pop()
 
-                    file_name = directories.pop()
-                    for directory in directories:
-                        file_path += f"{directory}/"
+                    if directories:
+                        file_name = directories.pop()
+                        for directory in directories:
+                            file_path += f"{directory}/"
 
-                    file_path += f"{file_name}.html"
-                    a['href'] = file_path
+                        file_path += f"{file_name}.html"
+                        a['href'] = file_path
 
         return soup.prettify()
+
+    def go_to_directory(self, url):
+        os.chdir('azrielifoundation.org')
+        directories = url[:-1].split('/')[3:]
+
+        if len(directories) in [0, 1]:
+            return
+        else:
+            directories.pop()
+            for directory in directories:
+                if not os.path.exists(directory):
+                    os.mkdir(directory)
+                os.chdir(directory)
+
+    def return_from_directory(self, url):
+        directories = url[:-1].split('/')[3:]
+
+        if len(directories) in [0, 1]:
+            return
+        else:
+            directories.pop()
+            for directory in range(len(directories)):
+                os.chdir('..')
 
     def save_file(self, file_name, content):
         with open(file_name, 'w') as html_file:
@@ -67,22 +86,21 @@ class AzrieliFoundationSpider(CrawlSpider):
 
     def parse_item(self, response):
         url = response.request.url
-        if ('/fr/' in url) or ('/he/' in url) or ('?' in url):
+        if ('/fr' in url) or ('/he' in url) or ('?' in url) or ('png' in url):
             return
+
+        os.chdir('..')
+        if not os.path.exists('azrielifoundation.org'):
+            os.mkdir('azrielifoundation.org')
 
         # Gets the file name for the pages
         file_name = self.get_file_name(url)
-        crawl_depth = response.meta['depth']
 
         # Get the content from the crawled pages
         page = requests.get(url)
         content = self.get_page_content(page.content.decode())
 
         # Saves the content in a directory corresponding to the website
+        self.go_to_directory(url)
         self.save_file(file_name, content)
-
-        yield {
-            "file name": file_name,
-            "url": url,
-            "depth": crawl_depth
-        }
+        self.return_from_directory(url)
