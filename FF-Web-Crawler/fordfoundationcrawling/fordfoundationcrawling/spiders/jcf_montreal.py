d1b3d3504de605b3e68bf3b99a44b84f9ce3f0b8
mollylia <mollyliao0803@gmail.com>
2024-03-22 13:30:32 -0700

    Saves crawled pages

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/jcf_montreal.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/jcf_montreal.py
@@ -1,8 +1,9 @@
 import os.path
+import requests
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-
+from bs4 import BeautifulSoup
 
 class JCFMontrealSpider(CrawlSpider):
     name = 'jcfmontreal'
@@ -35,6 +36,13 @@ class JCFMontrealSpider(CrawlSpider):
             file_name = f"{name_split.pop()}.html"
             return file_name
 
+    def get_page_content(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        for data in soup(['script', 'meta', 'style', 'img', 'video', 'button', 'input', 'iframe']):
+            data.decompose()
+
+        return soup.prettify()
+
     def parse_item(self, response):
         url = response.request.url
         if '/fr' in url:
@@ -42,6 +50,14 @@ class JCFMontrealSpider(CrawlSpider):
 
         # Gets the file name for the pages
         file_name = self.get_file_name(url)
+
+        # Get the content from the crawled pages
+        page = requests.get(url)
+        content = self.get_page_content(page.content.decode())
+
+        with open(file_name, 'w') as html_file:
+            html_file.write(str(content))
+
         crawl_depth = response.meta['depth']
 
         yield {
