fa035d50507fe8831106e27d9f65c9bd1d838850
mollylia <mollyliao0803@gmail.com>
2024-03-27 13:17:11 -0700

    Save page content

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/chagnon_foundation.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/chagnon_foundation.py
@@ -1,7 +1,10 @@
 import os.path
+import requests
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
+
 
 class ChagnonFoundationSpider(CrawlSpider):
     name = 'chagnon'
@@ -26,6 +29,13 @@ class ChagnonFoundationSpider(CrawlSpider):
             file_name = f"{name_split.pop()}.html"
             return file_name
 
+    def get_page_content(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        for data in soup(['script']):
+            data.decompose()
+
+        return soup.prettify()
+
     def parse_item(self, response):
         url = response.request.url
         if '?' in url:
@@ -34,6 +44,13 @@ class ChagnonFoundationSpider(CrawlSpider):
         # Gets the file name for the pages
         file_name = self.get_file_name(url)
         crawl_depth = response.meta['depth']
+
+        # Get the content from the crawled pages
+        page = requests.get(url)
+        content = self.get_page_content(page.content.decode())
+
+        with open(file_name, 'w') as html_file:
+            html_file.write(str(content))
 
         yield {
             "file name": file_name,
