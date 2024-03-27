f247851c82b50f01c97777c6643fd6c2d961b021
mollylia <mollyliao0803@gmail.com>
2024-03-27 11:25:54 -0700

    Saves page content

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/gift_funds.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/gift_funds.py
@@ -1,7 +1,9 @@
 import os.path
+import requests
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
 
 
 class SickKidsFoundationSpider(CrawlSpider):
@@ -15,6 +17,10 @@ class SickKidsFoundationSpider(CrawlSpider):
         os.mkdir('giftfunds.com')
     os.chdir('giftfunds.com')
 
+    custom_settings = {
+        'DEPTH_LIMIT': 1
+    }
+
     rules = (
         Rule(LinkExtractor(), callback='parse_item', follow=True),
     )
@@ -27,12 +33,26 @@ class SickKidsFoundationSpider(CrawlSpider):
             file_name = f"{name_split.pop()}.html"
             return file_name
 
+    def get_page_content(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        for data in soup(['script', 'meta', 'img', 'button', 'select']):
+            data.decompose()
+
+        return soup.prettify()
+
     def parse_item(self, response):
         url = response.request.url
 
         # Gets the file name for the pages
         file_name = self.get_file_name(url)
         crawl_depth = response.meta['depth']
+
+        # Get the content from the crawled pages
+        page = requests.get(url)
+        content = self.get_page_content(page.content.decode())
+
+        with open(file_name, 'w') as html_file:
+            html_file.write(str(content))
 
         yield {
             "file name": file_name,
