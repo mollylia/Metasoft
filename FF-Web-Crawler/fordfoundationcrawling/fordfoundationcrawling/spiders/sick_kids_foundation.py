775edb21b79ffd6cf2c3142f54736cb9fd59bc21
mollylia <mollyliao0803@gmail.com>
2024-03-28 15:41:47 -0700

    Save pages in directory corresponding to website

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/sick_kids_foundation.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/sick_kids_foundation.py
@@ -11,14 +11,8 @@ class SickKidsFoundationSpider(CrawlSpider):
     allowed_domains = ['sickkidsfoundation.com']
     start_urls = ['https://www.sickkidsfoundation.com/']
 
-    # TODO: move this to parse_item
-    os.chdir('..')
-    if not os.path.exists('sickkidsfoundation.com'):
-        os.mkdir('sickkidsfoundation.com')
-    os.chdir('sickkidsfoundation.com')
-
     custom_settings = {
-        'DEPTH_LIMIT': 1
+        'DEPTH_LIMIT': 5
     }
 
     rules = (
@@ -30,6 +24,9 @@ class SickKidsFoundationSpider(CrawlSpider):
             return "index.html"
         else:
             name_split = url.split('/')
+            if name_split[len(name_split)-1] == '':
+                name_split.pop()
+
             file_name = f"{name_split.pop()}.html"
             return file_name
 
@@ -40,10 +37,45 @@ class SickKidsFoundationSpider(CrawlSpider):
 
         return soup.prettify()
 
+    def go_to_directory(self, url):
+        os.chdir('sickkidsfoundation.com')
+        directories = url.split('/')[3:]
+        if directories[len(directories)-1] == '':
+            directories.pop()
+
+        if len(directories) in [0, 1]:
+            return
+        else:
+            directories.pop()
+            for directory in directories:
+                if not os.path.exists(directory):
+                    os.mkdir(directory)
+                os.chdir(directory)
+
+    def return_from_directory(self, url):
+        directories = url[:-1].split('/')[3:]
+        if directories[len(directories)-1] == '':
+            directories.pop()
+
+        if len(directories) in [0, 1]:
+            return
+        else:
+            directories.pop()
+            for directory in range(len(directories)):
+                os.chdir('..')
+
+    def save_file(self, file_name, content):
+        with open(file_name, 'w') as html_file:
+            html_file.write(str(content))
+
     def parse_item(self, response):
         url = response.request.url
         if ('?' in url) or ('https://www.sickkidsfoundation.com/' not in url):
             return
+
+        os.chdir('..')
+        if not os.path.exists('sickkidsfoundation.com'):
+            os.mkdir('sickkidsfoundation.com')
 
         # Gets the file name for the pages
         file_name = self.get_file_name(url)
@@ -53,8 +85,10 @@ class SickKidsFoundationSpider(CrawlSpider):
         page = requests.get(url)
         content = self.get_page_content(page.content.decode())
 
-        with open(file_name, 'w') as html_file:
-            html_file.write(str(content))
+        # Saves the content in a directory corresponding to the website
+        self.go_to_directory(url)
+        self.save_file(file_name, content)
+        self.return_from_directory(url)
 
         yield {
             "file name": file_name,
