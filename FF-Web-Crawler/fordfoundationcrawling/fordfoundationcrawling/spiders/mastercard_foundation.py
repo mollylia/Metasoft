49f652431b13deb2760983ceda452c95a3568847
mollylia <mollyliao0803@gmail.com>
2024-04-02 15:25:43 -0700

    Saved pages to directories corresponding to the website

--- FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/mastercard_foundation.py
+++ FF-Web-Crawler/fordfoundationcrawling/fordfoundationcrawling/spiders/mastercard_foundation.py
@@ -10,20 +10,6 @@ class MastercardFoundationSpider(scrapy.Spider):
     allowed_domains = ['mastercardfdn.org']
     start_urls = ['https://mastercardfdn.org/']
 
-    # TODO: move this to parse_item
-    os.chdir('..')
-    if not os.path.exists('mastercardfoundation.org'):
-        os.mkdir('mastercardfoundation.org')
-    os.chdir('mastercardfoundation.org')
-
-    # custom_settings = {
-    #     'DEPTH_LIMIT': 1
-    # }
-
-    # rules = (
-    #     Rule(LinkExtractor(allow_domains=allowed_domains), callback='parse_item', follow=True),
-    # )
-
     def start_requests(self):
         for url in self.start_urls:
             yield SeleniumRequest(url=url, callback=self.parse_item)
@@ -38,14 +24,50 @@ class MastercardFoundationSpider(scrapy.Spider):
 
     def get_page_content(self, html):
         soup = BeautifulSoup(html, 'html.parser')
-        for data in soup(['script']):
+        for data in soup(['script', 'meta', 'style', 'img', 'iframe', 'button', 'input', 'select']):
             data.decompose()
 
         return soup.prettify()
 
+    def go_to_directory(self, url):
+        os.chdir('mastercardfoundation.org')
+        directories = url[:-1].split('/')[3:]
+
+        if (len(directories) in [0, 1]) or (directories[0] == 'cdn-cgi'):
+            return
+        else:
+            directories.pop()
+
+        for directory in directories:
+            if not os.path.exists(directory):
+                os.mkdir(directory)
+            os.chdir(directory)
+
+    def return_from_directory(self, url):
+        directories = url[:-1].split('/')[3:]
+
+        if len(directories) in [0, 1]:
+            return
+        else:
+            directories.pop()
+
+        for directory in range(len(directories)):
+            os.chdir('..')
+
+    def save_file(self, file_name, content):
+        with open(file_name, 'w') as html_file:
+            html_file.write(str(content))
+
     def parse_item(self, response):
-        # Gets the file name for the pages
         url = response.request.url
+        if '?' in url:
+            return
+
+        os.chdir('..')
+        if not os.path.exists('mastercardfoundation.org'):
+            os.mkdir('mastercardfoundation.org')
+
+        # Gets the file name for the pages
         file_name = self.get_file_name(url)
         crawl_depth = response.meta['depth']
 
@@ -53,9 +75,10 @@ class MastercardFoundationSpider(scrapy.Spider):
         html_content = response.body
         content = self.get_page_content(html_content)
 
-        # Saves page content
-        with open(file_name, 'w') as html_file:
-            html_file.write(str(content))
+        # Saves the content in a directory corresponding to the website
+        self.go_to_directory(url)
+        self.save_file(file_name, content)
+        self.return_from_directory(url)
 
         yield {
             "file name": file_name,
